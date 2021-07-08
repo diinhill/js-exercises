ebdba13e0f2320c5574a00146c6f037c6743cee2
diinhill <hillenbrand.nadine@hotmail.de>
2021-07-08 13:13:07 +0200

    alternative version to duplicates exercise

--- main.js
+++ main.js
@@ -112,6 +112,27 @@ function duplicateArr (x) {
 console.log(duplicateArr(arr))
 
 
+/*third solution to exercise above*/
+
+var arr = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]
+
+const findDuplicate = (arr) => {
+    let counts = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (counts[arr[i]]) {
+            counts[arr[i]] += 1
+        } else counts[arr[i]] = 1
+    }
+    for (let el in counts) {
+        if (counts[el] >= 2) {
+            console.log(el)
+        }
+    }
+    console.log('counts', counts)
+}
+
+
+
 
 
 
